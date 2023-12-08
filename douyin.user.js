46929d013f8c177dabe368876564a82c9f24fce8
萌森 <qyg2297248353@163.com>
2023-12-08 22:56:30 +0800

    初始化

---
+++ douyin.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         抖音插件
+// @namespace    https://github.com/QYG2297248353/tampermonkey_douyin
+// @version      v1.0.0
+// @description  自动跳过广告
+// @author       新疆萌森软件开发工作室
+// @match        https://*.douyin.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        GM_log
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    GM_log("Hello Ms")
+})();
